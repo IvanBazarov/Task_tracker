8a52b77bc52bbf46370e55e75e9d96aa570fa572
Ivan <ivan.ab001@yandex.ru>
2018-07-23 14:12:49 +0300

    Initial Commit

---
+++ Task_tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task_tracker
+//
+//  Created by Иван Базаров on 23.07.2018.
+//  Copyright © 2018 Иван Базаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
